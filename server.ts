9249d202a734e3a6c9b2f06f144effedd32a26c0
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2024-06-06 22:06:22 -0300

    Set up routes

--- server.ts
+++ server.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+//import passport from 'passport'; 
 import connectDB from './db';
 
+import userRoutes from './routes/api/users';
+/*
+import nutritionistRoutes from './routes/api/nutritionists';
+import retailRoutes from './routes/api/retail';
+import anamnesesRoutes from './routes/api/anamneses';
+import anthropometryRoutes from './routes/api/anthropometry';
+*/
 const app = express();
 
 // Connect to Database
@@ -10,9 +18,16 @@ connectDB();
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//app.use(passport.initialize()); For future authentication strategies
 
-// Basic route
-app.get('/', (req, res) => res.send('API is running...'));
+// Routes
+app.use('/api/users', userRoutes);
+/*
+app.use('/api/nutritionists', nutritionistRoutes);
+app.use('/api/retail', retailRoutes);
+app.use('/api/anamneses', anamnesesRoutes);
+app.use('/api/anthropometry', anthropometryRoutes);
+*/
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
